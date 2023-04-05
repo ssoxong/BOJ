a7be70d24e1540c77681a9232083d369d2958739
ssoxong <112956015+ssoxong@users.noreply.github.com>
2023-04-06 01:28:32 +0900

    [level 2] Title: 짝지어 제거하기, Time: 50.35 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for(int i=1;i<s.length();i++){
+            if(stack.size()!=0&&stack.peek()==s.charAt(i))
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+        if(stack.size()==0)
+            return 1;
+        else
+            return 0;
+    }
+}
